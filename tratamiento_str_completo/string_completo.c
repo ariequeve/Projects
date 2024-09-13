3aa3678b9f2f27bb2caac6c4c29c7bd47b9f3b5d
Ariel <ariequeve@yahoo.com.ar>
2024-09-13 20:05:58 -0300

    Agrego correccion texto mensaje aprobacion

--- tratamiento_str_completo/string_completo.c
+++ tratamiento_str_completo/string_completo.c
@@ -80,9 +80,9 @@ int main() {
     /*3.B. Determinar si el promedio de las calificaciones es mayor o igual a 6.0 y, en ese caso, mostrar un mensaje
             que indique si el estudiante aprobó o reprobó el curso. */
     if (promedio > 6) {
-        printf("El Alumno APROBO el examen \n");
+        printf("El Alumno APROBO la materia \n");
     } else {
-        printf("El Alumno NO APROBO el examen \n");
+        printf("El Alumno NO APROBO la materia \n");
     }
     
     return 0;
